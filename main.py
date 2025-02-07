2f16352ae6abd8a9e4c5157ca6e22fd61de460ae
Basov Aleksey <basovaleks17s@mail.ru>
2025-02-07 14:09:44 +0300

    method delete

--- main.py
+++ main.py
@@ -321,5 +321,17 @@ def get_users(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
+# Delete
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    db.delete(user)  
+    db.commit()      
+    return {"detail": "User deleted successfully"}
+
 
 
